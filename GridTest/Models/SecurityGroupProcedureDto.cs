065bc179aeb932fed84a5cc216d3d78ef491aca9
Steve <spospisil109@gmail.com>
2022-07-12 11:43:04 -0400

    Initial Commit

---
+++ GridTest/Models/SecurityGroupProcedureDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GridTest.Models
+{
+    [Serializable]
+    public class SecurityGroupProcedureDto 
+    {
+        public Guid? TenantId { get; set; }
+        public Guid SecurityGroupId { get; set; }
+        public Guid ProcedureId { get; set; }
+        public bool? Status { get; set; }
+    }
+}
